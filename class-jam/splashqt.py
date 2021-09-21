551165289e89692524df88cd088047f5f87de7ae
juanma1980 <juanma1980@gmail.com>
2021-09-21 18:04:49 +0200

    add some qt window flags

--- class-jam/splashqt.py
+++ class-jam/splashqt.py
@@ -4,7 +4,12 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import QSize,Qt
 
 app = QApplication([])
-pixmap = QtGui.QPixmap("class-jam/splash.png")
+pixmap = QtGui.QPixmap("/usr/share/class-jam/splash.png")
 splash = QSplashScreen(pixmap)
+splash.setWindowFlags(Qt.WindowStaysOnTopHint)
+splash.setWindowFlags(Qt.FramelessWindowHint)
+splash.setWindowFlags(Qt.X11BypassWindowManagerHint)
+splash.setAttribute(Qt.WA_TranslucentBackground)
+splash.setStyleSheet('background:transparent')
 splash.show()
 app.exec_()
